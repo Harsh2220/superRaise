4eca4977bca8028501079888f42137769e395655
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-06-27 10:09:01 +0530

    fix:upload metadata api

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,17 +1,18 @@
+import { metadata } from "./../../layout";
 import { APP_ID } from "../../../constants/index";
 import { NextResponse, NextRequest } from "next/server";
 import Bundlr from "@bundlr-network/client";
 
 export async function POST(req: NextRequest) {
   try {
-    const { metadata } = await req.json();
+    const data = await req.json();
     const bundlr = new Bundlr(
       "http://node2.bundlr.network",
       "matic",
       process.env.PRIVATE_KEY
     );
     await bundlr.ready();
-    const tx = await bundlr.upload(JSON.stringify(metadata), {
+    const tx = await bundlr.upload(JSON.stringify(data), {
       tags: [
         { name: "Content-Type", value: "application/json" },
         { name: "App-Name", value: APP_ID },
@@ -19,6 +20,7 @@ export async function POST(req: NextRequest) {
     });
     return NextResponse.json({ uri: `ar://${tx.id}` });
   } catch (error) {
-    NextResponse.json("Something went wrong");
+    console.log("eeeeeee", error);
+    return NextResponse.json("Something went wrong");
   }
 }
